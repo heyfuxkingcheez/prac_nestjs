afca3a6a0cb897617a0aefc0dcc90951db27ae95
woogi-on_and-on <jgw987@gmail.com>
2024-05-28 00:15:45 +0900

    feat [auth] Implement BasicTokenGaurd, AccessTokenGuard, and RefreshTokenGuard Created BasicTokenGuard for Basic token authentication Added BearerTokenGuard to handle bearer token verification Extended BearerTokenGuard to create AccessTokenGuard and RefreshTokenGuard Enhanced security by validating token types and ensuring proper user authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { Body, Controller, Headers, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserReqDto } from './dto/createUser.req.dto';
+import { PasswordPipe } from './pipe/password.pipe';
+import { BsaicTokenGuard } from './guard/basic-token.guard';
+import { RefreshTokenGuard } from './guard/bearer-token.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('token/access')
+  @UseGuards(RefreshTokenGuard)
   postTokenAccess(@Headers('authorization') rawToken: string) {
     const token = this.authService.extractTokenFromHeader(rawToken, true);
 
@@ -18,6 +22,7 @@ export class AuthController {
   }
 
   @Post('token/refresh')
+  @UseGuards(RefreshTokenGuard)
   postTokenRefresh(@Headers('authorization') rawToken: string) {
     const token = this.authService.extractTokenFromHeader(rawToken, true);
 
@@ -29,6 +34,7 @@ export class AuthController {
   }
 
   @Post('login')
+  @UseGuards(BsaicTokenGuard)
   postLoginEamil(@Headers('authorization') rawToken: string) {
     const token = this.authService.extractTokenFromHeader(rawToken, false);
 
@@ -39,6 +45,11 @@ export class AuthController {
 
   @Post('register')
   postRegisterEmail(@Body() dto: CreateUserReqDto) {
+    const passwordValidationPipe = new PasswordPipe();
+    dto.password = passwordValidationPipe.transform(dto.password, {
+      type: 'body',
+      data: 'password',
+    });
     return this.authService.registerWithEmail(dto);
   }
 }
